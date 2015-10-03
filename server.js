80649f7f77c5e9b3c688caa67020ce27762cf620
marchchad <cmarch@resdat.com>
2015-10-03 08:11:45 -0700

    modified port

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.use(function(err, req, res, next) {
  * Get port from environment and store in Express.
  */
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '8080');
 app.set('port', port);
 
 /**
