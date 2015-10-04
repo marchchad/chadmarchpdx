e685bb9a366acbbdc35ee0fa1fe83f843fc6d717
marchchad <cmarch@resdat.com>
2015-10-03 17:43:21 -0700

    restored import of routes module.

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var debug = require('debug')('mynodeapp:server');
 var http = require('http');
 
+var routes = require('./routes/index');
+//var users = require('./routes/users');
+
 var app = express();
 
 // view engine setup
