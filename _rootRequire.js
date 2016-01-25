59ef9a08f55b73ef7e29896da4e31bba0710508b
marchchad <cmarch@resdat.com>
2016-01-25 10:21:56 -0800

    fixed path references.

--- _rootRequire.js
+++ _rootRequire.js
@@ -5,7 +5,7 @@
  * @return {Object}
  */
 var rootRequire = function(module) {
-  module = module.indexOf("\\") === -1 ? "\\" + module : module;
+  module = module.indexOf("/") === -1 ? "/" + module : module;
   return require(__dirname + module);
 }
 module.exports = GLOBAL._rootRequire = rootRequire;
